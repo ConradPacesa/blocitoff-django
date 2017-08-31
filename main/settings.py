d2d59d6eede11dfc1449d39cd7233400291f5921
Conrad Pacesa <conradpacesa@gmail.com>
2017-08-30 21:11:33 -0400

    modded settings

--- main/settings.py
+++ main/settings.py
@@ -74,7 +74,7 @@ WSGI_APPLICATION = 'main.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
+    'default': dj_database_url.os.environ.get(
         default=os.environ.get('DATABASE_URL')
     )
 }
