c70f31ec6f0899c0e54cb24aa72971d5cd902342
Conrad Pacesa <conradpacesa@gmail.com>
2017-08-30 23:07:37 -0400

    Created Item model, views, serializer

--- blocitoff/models.py
+++ blocitoff/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Item(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     name = models.CharField(max_length=100, blank=False)
+    owner = models.ForeignKey('auth.User', related_name='items', on_delete=models.CASCADE)
 
     class Meta:
-        ordering = ('created')
+        ordering = ('created',)
--- blocitoff/views.py
+++ blocitoff/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from blocitoff.models import Item
+from django.contrib.auth.models import User
+from blocitoff.serializers import ItemSerializer, UserSerializer
+from rest_framework import generics, permissions, renderers, viewsets
+from rest_framework.decorators import api_view, detail_route
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 
-# Create your views here.
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'items': reverse('item-list', request=request, format=format)
+    })
+
+class ItemViewSet(viewsets.ModelViewSet):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
+
+    @detail_route(renderer_classes=[renderers.StaticHTMLRenderer])
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
